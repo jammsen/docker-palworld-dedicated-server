d6ca79466fa5e474e61eb4786c2bfba120a8e1f7
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2024-01-29 11:59:01 +0100

    #122 added native rcon/rconcli binary to enable other feature request to be able to work

--- backupmanager.sh
+++ backupmanager.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d_%H%M%S)
+TIME=$(date +%H-%M-%S)
 
+echo ">>> Creating backup"
+echo "> Sending message to gameserver"
+cd ~/steamcmd/
+rconcli "broadcast $TIME-Backup_in_progress"
+sleep 1
+rconcli 'broadcast Saving...'
+rconcli 'save'
+rconcli 'broadcast Done...'
+sleep 15
 # Create backup dir and change into it
 mkdir -p /palworld/backups && cd /palworld/Pal
-echo ">>> Creating backup"
 tar cfz /palworld/backups/saved-$DATE.tar.gz Saved/
 echo ">>> Done"
