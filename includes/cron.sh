53469f6cdc1843d64a54b52a09dda8e2b7e1d98a
Sebastian Schmidt <2270806+jammsen@users.noreply.github.com>
2024-02-01 11:12:57 +0100

    Resolves #161 - Fixed having the same entry over and over again on docker-compose restart

--- includes/cron.sh
+++ includes/cron.sh
@@ -1,6 +1,6 @@
 # Function to start supercronic and load crons from cronlist
 function setup_crons() {
-    touch cronlist
+    echo "" > cronlist
     if [[ -n $BACKUP_ENABLED ]] && [[ $BACKUP_ENABLED == "true" ]]; then
         echo "$BACKUP_CRON_EXPRESSION /backupmanager.sh" >> cronlist
     fi
