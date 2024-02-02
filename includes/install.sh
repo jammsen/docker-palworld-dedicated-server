a02825eb789cbe20e86d914838600af631afeb74
Jason Bristol <json.bristol@gmail.com>
2024-02-01 23:12:01 -0500

    Fix typos in webhook messages

--- includes/install.sh
+++ includes/install.sh
@@ -3,7 +3,7 @@ function install_server() {
     # force a fresh install of all
     echo ">>> Doing a fresh install of the gameserver"
     if [[ -n $WEBHOOK_ENABLED ]] && [[ $WEBHOOK_ENABLED == "true" ]]; then
-        send_webhook_notification "Installing server" "Server is beeing installed" "$WEBHOOK_INFO_COLOR"
+        send_webhook_notification "Installing server" "Server is being installed" "$WEBHOOK_INFO_COLOR"
     fi
     /home/steam/steamcmd/steamcmd.sh +force_install_dir "$GAME_PATH" +login anonymous +app_update 2394010 validate +quit
 }
@@ -14,13 +14,13 @@ function update_server() {
     if [[ -n $STEAMCMD_VALIDATE_FILES ]] && [[ $STEAMCMD_VALIDATE_FILES == "true" ]]; then
         echo ">>> Doing an update and validate of the gameserver files"
         if [[ -n $WEBHOOK_ENABLED ]] && [[ $WEBHOOK_ENABLED == "true" ]]; then
-            send_webhook_notification "Updating server" "Server is beeing updated and validated" "$WEBHOOK_INFO_COLOR"
+            send_webhook_notification "Updating server" "Server is being updated and validated" "$WEBHOOK_INFO_COLOR"
         fi
         /home/steam/steamcmd/steamcmd.sh +force_install_dir "$GAME_PATH" +login anonymous +app_update 2394010 validate +quit
     else
         echo ">>> Doing an update of the gameserver files"
         if [[ -n $WEBHOOK_ENABLED ]] && [[ $WEBHOOK_ENABLED == "true" ]]; then
-            send_webhook_notification "Updating server" "Server is beeing updated" "$WEBHOOK_INFO_COLOR"
+            send_webhook_notification "Updating server" "Server is being updated" "$WEBHOOK_INFO_COLOR"
         fi
         /home/steam/steamcmd/steamcmd.sh +force_install_dir "$GAME_PATH" +login anonymous +app_update 2394010 +quit
     fi
