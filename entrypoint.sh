277596a9d4ad7224f3ef78fe0495c436dcda6e37
João Palma <joaocarlospalma.io@gmail.com>
2024-02-08 19:42:15 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 
 set -e
-# shellcheck disable=SC1091
+
 source /includes/colors.sh
 
 
 if [[ "${PUID}" -eq 0 ]] || [[ "${PGID}" -eq 0 ]]; then
-    ew "[WARNING] Running as root is not supported, please fix your PUID and PGID!\n"
+    ew ">> Running as root is not supported, please fix your PUID and PGID!\n"
     exit 1
 elif [[ "$(id -u steam)" -ne "${PUID}" ]] || [[ "$(id -g steam)" -ne "${PGID}" ]]; then
-    ei "Current steam user UID is '$(id -u steam)' and GID is '$(id -g steam)'\n"
-    ei "Setting new steam user UID to '${PUID}' and GID to '${PGID}'\n"
+    ei "> Current steam user UID is '$(id -u steam)' and GID is '$(id -g steam)'\n"
+    ei "> Setting new steam user UID to '${PUID}' and GID to '${PGID}'\n"
     groupmod -g "${PGID}" steam && usermod -u "${PUID}" -g "${PGID}" steam
 fi
 
