34b7c1748b68b143fad6f8bd711fb9dbdde53edd
StaleLoafOfBread <45444205+StaleLoafOfBread@users.noreply.github.com>
2024-02-27 22:10:16 -0500

    fix(typo): thread -> threat

--- includes/security.sh
+++ includes/security.sh
@@ -5,11 +5,11 @@ source /includes/colors.sh
 function check_for_default_credentials() {
     e "> Checking for existence of default credentials"
     if [[ -n $ADMIN_PASSWORD ]] && [[ $ADMIN_PASSWORD == "adminPasswordHere" ]]; then
-        ee ">>> Security thread detected: Please change the default admin password. Aborting server start ..."
+        ee ">>> Security threat detected: Please change the default admin password. Aborting server start ..."
         exit 1
     fi
     if [[ -n $SERVER_PASSWORD ]] && [[ $SERVER_PASSWORD == "serverPasswordHere" ]]; then
-        ee ">>> Security thread detected: Please change the default server password. Aborting server start ..."
+        ee ">>> Security threat detected: Please change the default server password. Aborting server start ..."
         exit 1
     fi
     es "> No default passwords found"
